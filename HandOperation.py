e7aa7fbf3c4043ab5e0418ae94feedd53a18c576
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-06-19 23:40:05 +0530

    add operations

---
+++ HandOperation.py
@@ -0,0 +1,22 @@
+
+class handDetector():
+    
+    def __init__(self, lmList):
+        self.lmList = lmList
+        
+    def findhandIstoClose(self):
+        result =  self.lmList[0][2] - self.lmList[12][2]
+        result2 = self.lmList[11][2] - self.lmList[12][2]
+        if(result > 175 and result2 >= 0):
+            return True
+        else:
+            return False
+    def findhandIstoFar(self):
+        result =  self.lmList[0][2] - self.lmList[12][2]
+        result2 = self.lmList[11][2] - self.lmList[12][2]
+        if(result < 100 and result2 >= 0):
+            return True
+        else:
+            return False
+            
+        
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import os
 import HandTrackingModule as htm
+import HandOperation as op
 
 top, right, bottom, left = 10, 350, 225, 590
 
@@ -9,18 +10,32 @@ cap = cv2.VideoCapture(0)
 #cap.set(3, wCam)
 #cap.set(4, hCam)
 
-detector = htm.handDetector(detectionCon=0.75)
+detector = htm.handDetector(detectionCon=0.80)
 
 #tipIds = [4, 8, 12, 16, 20]
-
+font = cv2.FONT_HERSHEY_SIMPLEX
+result = 0;
 while True:
     ret, img = cap.read()
     img=cv2.flip(img,1)
     img=cv2.rectangle(img, (left, top), (right, bottom), (0,255,0), 2)
     roi = img[top:bottom, right:left]
+    
     roi = detector.findHands(roi)
+    
     lmList = detector.findPosition(roi, draw=False)
-
+    
+    if len(lmList) != 0:
+        operation = op.handDetector(lmList)
+        fingers = []
+        #check hand distance
+        isToClose = operation.findhandIstoClose()
+        if(isToClose == True):
+            cv2.putText(img,'Hand too close',(0,50), font, 1, (0,0,255), 2, cv2.LINE_AA)
+        isSoFar = operation.findhandIstoFar()
+        if( isSoFar == True):
+            cv2.putText(img,'Hand is in so far',(0,50), font, 1, (0,0,255), 2, cv2.LINE_AA)
+        
     cv2.imshow("Image", img)
   
     if cv2.waitKey(1) & 0xFF == ord('q'):
