551c36e0018e5fe6cbd976d172c9bd09eda61c35
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-06-20 19:22:47 +0530

    updated

--- HandOperation.py
+++ HandOperation.py
@@ -1,4 +1,5 @@
-
+import math
+turnList = ["left", "right", "gohead"]
 class handDetector():
     
     def __init__(self, lmList):
@@ -21,14 +22,79 @@ class handDetector():
         else:
             return False
     def findStop(self):
-        thumb = self.lmList[4][1] - self.lmList[2][1] 
+        #thumb = self.lmList[4][1] - self.lmList[2][1] 
         index = self.lmList[8][2] - self.lmList[5][2]
         middle = self.lmList[12][2] - self.lmList[9][2]
         ring = self.lmList[16][2] - self.lmList[13][2]
         pinky = self.lmList[20][2] - self.lmList[17][2]
-        if(thumb > 0 and index > 0 and middle > 0 and ring > 0 and pinky > 0):
+        if(index > 0 and middle > 0 and ring > 0 and pinky > 0):
             return True
         else:
-            return False    
+            return False
+    
+    def calculateAngle(self):
+        fingerList = [4, 8, 12, 16]
+        returnList = []
+        for i in fingerList:
+            a = math.sqrt((self.lmList[i][1] - self.lmList[i+4][1])**2 + (self.lmList[i][2] - self.lmList[i+4][2])**2)
+            b = math.sqrt((self.lmList[0][1] - self.lmList[i+4][1])**2 + (self.lmList[0][2] - self.lmList[i+4][2])**2)
+            c = math.sqrt((self.lmList[i][1] - self.lmList[0][1])**2 + (self.lmList[i][2] - self.lmList[0][2])**2)
+            #s = (a+b+c)/2
+            #ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
+            #d =(2*ar)/a
+            
+            # apply cosine rule here
+            angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 100
+            returnList.append(int(angle))
+        return returnList
+    
+    def turningDirection(self):
+        if((self.lmList[8][1] - self.lmList[0][1]) > 45):
+           # print(self.lmList[8][1] - self.lmList[0][1])
+            return turnList[1]
+        elif((self.lmList[0][1] - self.lmList[16][1]) > 45):
+            #print(self.lmList[0][1] - self.lmList[16][1])
+            return turnList[0]
+        else:
+            return turnList[2]
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
             
         
--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import HandTrackingModule as htm
 import HandOperation as op
 
 top, right, bottom, left = 10, 350, 225, 590
+maximemangle = [60, 15, 13, 20]
+turnList = ["left", "right", "gohead"]
 
 cap = cv2.VideoCapture(0)
 
 detector = htm.handDetector(detectionCon=0.75)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
-result = 0;
+result = 0
 while True:
     ret, img = cap.read()
     img=cv2.flip(img,1)
@@ -24,6 +26,7 @@ while True:
     
     if len(lmList) != 0:
         operation = op.handDetector(lmList)
+        
         fingers = []
         #check hand distance
         isToClose = operation.findhandIstoClose()
@@ -36,8 +39,47 @@ while True:
         if( isStop == True):
             cv2.putText(img,'Stop',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
         else:
-            cv2.putText(img,'Go head',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
+            angleResult = operation.calculateAngle()
+            #print(angleResult)
+            if(angleResult[0] > maximemangle[0] and angleResult[1] > maximemangle[1] 
+              # and angleResult[2] > maximemangle[2] 
+               and angleResult[3] > maximemangle[3]):
+                cv2.putText(img,'Reverse',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
+            else:
+                if(angleResult[0] > maximemangle[0] and angleResult[1] < maximemangle[1] 
+               and angleResult[2] < maximemangle[2] and angleResult[3] < maximemangle[3]):
+                    cv2.putText(img,'Slow down',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
+                    direction =  operation.turningDirection()
+                    if(direction == turnList[0]):
+                        cv2.putText(img,'left',(0,110), font, 1, (0,0,255), 2, cv2.LINE_AA)
+                    elif(direction == turnList[1]):
+                        cv2.putText(img,'Right',(0,110), font, 1, (0,0,255), 2, cv2.LINE_AA)
+                    elif(direction == turnList[2]):
+                        cv2.putText(img,'Go head',(0,110), font, 1, (0,0,255), 2, cv2.LINE_AA)
+                else:
+                    direction =  operation.turningDirection()
+                    if(direction == turnList[0]):
+                        cv2.putText(img,'left',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
+                    elif(direction == turnList[1]):
+                        cv2.putText(img,'Right',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
+                    elif(direction == turnList[2]):
+                        cv2.putText(img,'Go head',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
+            #elif(angleResult[0] < maximemangle[0] and angleResult[1] < maximemangle[1] 
+             #  and angleResult[2] < maximemangle[2] and angleResult[3] < maximemangle[3]):
+              #  cv2.putText(img,'Go head',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
+            
+            #elif():
+                
+                
+            
+            #for i in range(4):
+             #    if( angleResult[i] > maximemangle[i]):
+              #      print(str(i) + ' angle ok')
+                
+                
+            #cv2.putText(img,'Go head',(0,80), font, 1, (0,0,255), 2, cv2.LINE_AA)
         
+         
     cv2.imshow("Image", img)
   
     if cv2.waitKey(1) & 0xFF == ord('q'):
